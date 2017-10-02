47885c2cc24afa484611eb2c749f6ea7ca084eab
Argir Argirov <dj.giro@gmail.com>
2017-10-02 16:13:54 -0400

    completed

--- data.sql
+++ data.sql
@@ -15,14 +15,14 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
 --
 
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
 --
 
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
@@ -531,6 +531,7 @@ COPY persons (id, name, age, residence_id) FROM stdin;
 \.
 
 
+
 --
 -- Name: persons_id_seq; Type: SEQUENCE SET; Schema: public; Owner: natalie
 --
@@ -739,4 +740,3 @@ ALTER TABLE ONLY residences
 --
 -- PostgreSQL database dump complete
 --
-
